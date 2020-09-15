0648a66b94aa84fe52f2775d9c52f26d7c2d9b8b
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-15 08:26:59 +0200

    Adding imperial method to the test in user_can_calculate_theirBMI.spec.js

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -1,9 +1,23 @@
+<reference types="Cypress" />
+
 describe('BMI Calculator', () => {
   it('Calculates BMI in metric', () => {
-    cy.visit('/');
+    beforeEach(() => {
+      cy.visit('/');
+  })
+   
+  it('Calculates BMI in metric', () => { 
     cy.get('input#weight').type(90);
     cy.get('input#height').type(190);
     cy.get('button#calculate').click();
     cy.get('p#bmi-message').should('contain', 'You are Normal with a BMI of 24.93')
   })
+
+  it('Calculates BMI in imperial',() => {
+    cy.get('select#select-metohd').select('imperial');
+    cy.get('input#weight').type(198);
+    cy.get('input"height').type(75);
+    cy.get('button#calculate').click();
+    cy.get('p#bmi-message').should('contain', 'You are Overweight with a BMI of 25.42')
+  })  
 })
