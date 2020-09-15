9793befa54074d09f3710791f4238e2566262cee
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-09-15 09:29:07 +0200

    Corrects spelling mistake in message.spec.js, line: 10.

--- src/__tests__/Message.spec.js
+++ src/__tests__/Message.spec.js
@@ -7,7 +7,7 @@ describe("Message component", () => {
   const wrapper = shallow(<Message bmiMessage="Normal" bmiValue="23.59" />);
 
   it ("renders with message prop", () => {
-    expect(wrapper.find("#bmi-message")text()).toEqual(
+    expect(wrapper.find("#bmi-message").text()).toEqual(
       "You are Normal with a BMI of 23.59"
     );
   });
